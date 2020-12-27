d7ec9a7509fdc30caa348a0be2ec70a6c704af77
zeka <2667286947@qq.com>
2020-12-28 01:39:15 +0800

    Add STEAM page

--- src/components/page/steam/steam-component.tsx
+++ src/components/page/steam/steam-component.tsx
@@ -1,8 +1,90 @@
 import React from "react";
 
-const SteamComponent = (): JSX.Element => {
-    return (<>
-    </>);
+const STEAM_BASE_URL = 'https://steamcommunity.com/app'
+const STEAM_IMAGE_URL = 'https://steamcdn-a.akamaihd.net/steam/apps'
+
+const getSteamUrl = (appId: string | number): string => {
+
+    return `${STEAM_BASE_URL}/${appId}`;
+};
+
+const getSteamImageUrl = (appId: string | number): string => {
+
+    return `${STEAM_IMAGE_URL}/${appId}/header.jpg`;
+};
+
+const num2time = (numTime: string | number): ITime => {
+    let t: number;
+    const myTime: ITime = {
+        hour: -1,
+        minute: -1,
+    }
+
+    if (typeof numTime === 'string') {
+        t = parseInt(numTime);
+    } else {
+        t = numTime;
+    }
+
+    if (t < 60) {
+        myTime.minute = t;
+    } else {
+        myTime.hour = Math.floor(t / 60);
+        myTime.minute = t % 60;
+    }
+    return myTime;
+}
+
+const SteamComponent = ({appId, name, time, recentTime = null}: ISteamComponentProp): JSX.Element => {
+
+    const myTime = num2time(time);
+    const myRecentTime = num2time(recentTime);
+    let recentTimer: JSX.Element;
+    let timer: JSX.Element;
+    const url = getSteamUrl(appId);
+    const imgSrc = getSteamImageUrl(appId);
+
+    if (myTime.hour === -1) {
+        timer = (<p>总共玩了 {myTime.minute}m</p>);
+    } else {
+        timer = (<p>总共玩了 {myTime.hour}h{myTime.minute}m</p>);
+    }
+
+    if (recentTime !== null) {
+        if (myRecentTime.hour === -1) {
+            recentTimer = (<p>最近玩了 {myRecentTime.minute}m</p>);
+        } else {
+            recentTimer = (<p>最近玩了 {myRecentTime.hour}h{myRecentTime.minute}m</p>);
+        }
+    }
+
+
+    return (
+        <div className="my-steam-cp card">
+            <a target="_blank" href={ url }>
+                <div className="my-steam-logo">
+                    <img src={ imgSrc } alt={ appId.toString() } />
+                </div>
+                <div className="my-steam-info">
+                    <h2><a href="">{ name }</a></h2>
+                    {recentTimer}
+                    {timer}
+                </div>
+            </a>
+        </div>
+    );
 };
 
 export default SteamComponent;
+
+interface ITime {
+    hour: number
+    minute: number
+}
+
+export interface ISteamComponentProp {
+    appId: number | string
+    name: string
+    time: string | number
+    recentTime?: string | number | null
+}
--- src/pages/steam.tsx
+++ src/pages/steam.tsx
@@ -1,16 +1,79 @@
-import React from "react";
-
-import { Link } from "gatsby";
+import React, {Dispatch, useEffect, useState} from "react";
 
 import SEO from "../components/public/seo";
 import Header from "../components/public/header";
+import SteamComponent from "../components/page/steam/steam-component";
+
+const STEAM_ALL_URL = 'https://steaminfo.mmx223223.workers.dev';
+const STEAM_RECENT_URL = 'https://steaminfo.mmx223223.workers.dev?type=recent';
+
+const fetchData = (url: string, setter: Dispatch<ISteamData>) => {
+    fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+    }).then(response => response.json()).then((data: ISteamData) => {
+        setter(data);
+    }).catch(e=>{
+        console.log(e);
+    })
+}
+
+const initData: ISteamData = {
+    response: {
+        games:[],
+    }
+};
 
 const SteamPage = (): JSX.Element => {
+
+    const [ data, setData ] = useState<ISteamData>(initData);
+    const [ recentData, recentSetData ] = useState<ISteamData>(initData);
+    const games = data.response.games;
+    const recentGames = recentData.response.games;
+
+    useEffect( ()=>{
+        fetchData(STEAM_ALL_URL, setData);
+    }, [])
+
+    useEffect( ()=>{
+        fetchData(STEAM_RECENT_URL, recentSetData);
+    }, [])
+
     return (<>
         <SEO title="Zeka 的 Steam" />
         <Header title="Steam" />
-        <Link to="/">返回首页</Link>
+        <div className="my-steam">
+            <div className="my-steam__intro">
+                <p>最近在玩({recentGames.length})</p>
+            </div>
+            <div className="my-steam-part my-steam-recently">
+                {recentGames.map(game=>{
+                    return (<SteamComponent key={game.appid} appId={game.appid} name={game.name} time={game.playtime_forever} recentTime={game.playtime_2weeks} />);
+                })}
+            </div>
+            <div className="my-steam__intro">
+                <p>我的库存({games.length})</p>
+            </div>
+            <div className="my-steam-part my-steam-all">
+                {games.map(game=>{
+                    return (<SteamComponent key={game.appid} appId={game.appid} name={game.name} time={game.playtime_forever} />);
+                })}
+            </div>
+        </div>
     </>);
 };
 
 export default SteamPage;
+
+interface ISteamData {
+    response:{
+        games: ISteamItem[]
+    }
+}
+
+interface ISteamItem {
+    appid: number
+    name: string
+    playtime_2weeks?: number
+    playtime_forever: number
+}
