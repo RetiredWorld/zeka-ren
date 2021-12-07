a12c68b59a02ef279640885093f7e7d0cab1fc76
RetiredWorld <mmx223223@gmail.com>
2021-12-07 22:52:21 +0800

    Fix wrong year time

--- src/components/public/header.tsx
+++ src/components/public/header.tsx
@@ -10,7 +10,7 @@ export interface HeaderProp {
 function yearPastPercent(): number{
     const year = new Date().getFullYear();
     const tDay = ( year%4==0 && year%100!=0 || year%400==0 )? 366 : 365;
-    const n1 = Date.parse((new Date(year,1-1,1)).toDateString());
+    const n1 = Date.parse((new Date(year,0,0)).toDateString());
     const n2 = Date.parse((new Date()).toDateString());
     const pDay = (n2-n1)/(1000 * 60 * 60 * 24);
     return 100 - Math.ceil(pDay/tDay*10000)/100;
@@ -21,8 +21,8 @@ const Header: React.FC<HeaderProp> = (prop) => {
     const [ percent ] = useState(yearPastPercent());
 
     return (<header className="my-header">
-            <h1 ref={headerRef} className="my-header-title" time-str={`今年还剩 ${percent}%`} style={{
-                [ '--time-percent' as any]: percent / 100,
+            <h1 ref={headerRef} className="my-header-title" time-str={`今年还剩 ${percent.toFixed(2)}%`} style={{
+                [ '--time-percent' as any]: (percent / 100).toFixed(2),
             }}><Link className="my-header__link" to="/">Zeka 的记事本</Link></h1>
             <span className="my-header-sub">{prop.title}</span>
     </header>);
